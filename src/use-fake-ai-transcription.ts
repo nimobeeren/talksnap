5201ae44f6eb29dbfa9ebdcc8b22fedc52e72e33
Nimo Beeren <nimobeeren@gmail.com>
2024-07-31 21:34:54 +0200

    handle error when destroying session while streaming results

--- src/use-fake-ai-transcription.ts
+++ src/use-fake-ai-transcription.ts
@@ -1,4 +1,4 @@
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { TranscriptionResult } from "./use-transcription";
 
 type AITextSession = any;
@@ -10,43 +10,70 @@ declare global {
 }
 
 export function useFakeAiTranscription({
-  onResult,
   speed,
+  onResult,
 }: {
-  onResult?: (results: TranscriptionResult[]) => void;
+  /**
+   * Maximum number of chunks per second. If this is faster than the AI can
+   * generate, results will appear at the rate the AI generates them.
+   */
   speed: number;
+  /**
+   * Callback for when a new transcription result arrives. This is called with
+   * all results since starting the transcription.
+   */
+  onResult?: (results: TranscriptionResult[]) => void;
 }): {
   start: () => void;
   stop: () => void;
 } {
   const [aiSession, setAiSession] = useState<AITextSession>(null);
 
-  const startTranscription = useCallback(async () => {
-    const session = await window.ai.createTextSession();
-    setAiSession(session);
+  useEffect(() => {
+    return () => {
+      aiSession?.destroy();
+    };
+  }, [aiSession]);
+
+  const start = useCallback(async () => {
+    let session: any;
+    if (aiSession) {
+      session = aiSession;
+    } else {
+      session = await window.ai.createTextSession();
+      setAiSession(session);
+    }
 
     const stream = session.promptStreaming(
       "You are a conference speaker. Give a talk on software testing.",
     );
 
-    for await (const chunk of stream) {
-      const processedChunk = String(chunk).replaceAll("\n", " ");
-      onResult?.([
-        {
-          transcript: processedChunk,
-          isFinal: true,
-        },
-      ]);
+    try {
+      for await (const chunk of stream) {
+        const processedChunk = String(chunk).replaceAll("\n", " ");
+        onResult?.([
+          {
+            transcript: processedChunk,
+            isFinal: true,
+          },
+        ]);
+      }
+    } catch (e) {
+      if (e instanceof DOMException && e.name === "InvalidStateError") {
+        // This is okay, happens when destroying the session while streaming results
+      } else {
+        throw e;
+      }
     }
-  }, [onResult]);
+  }, [onResult, aiSession]);
 
-  const stopTranscription = useCallback(() => {
+  const stop = useCallback(() => {
     aiSession?.destroy();
     setAiSession(null);
   }, [aiSession]);
 
   return {
-    start: startTranscription,
-    stop: stopTranscription,
+    start,
+    stop,
   };
 }
